874f5be396887ba17980891f86ced26f824e3d97
daysantace <daysantace@gmail.com>
2022-08-29 08:52:11 +0100

    improve bios  or something

--- src/startup/start.lua
+++ src/startup/start.lua
@@ -1,6 +1,3 @@
-<<<<<<< HEAD:bios.lua
-local result, reason = ""
-=======
 -- components needed
 local function getComponentAddress(name)
 	return component.list(name)() or error("ERROR! Component " .. name .. " is missing")
@@ -16,4 +13,5 @@ local eepromAddress, modemAddress, gpuAddress, screenAddress, cpuAddress =
 -- bind gpu
 component.invoke(gpuAddress, "bind", screenAddress")
 local screenWidth, screenHeight = component.invoke(gpuAddress, "getResolution")
->>>>>>> 927efbb70286e41fa9ddef4002583e38b3dc6661:src/bios.lua
+
+print("Hello, World!")
