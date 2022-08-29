8c5a532ac893b8b50814a2e79938c0205b1500a4
daysantace <daysantace@gmail.com>
2022-08-29 11:04:34 +0100

    brckaet fix 2

--- bios.lua
+++ bios.lua
@@ -1,4 +1,4 @@
 local result, reason = ""
 do
-	local handle, chunk = component.proxy(component.list("internet")) or error("ERROR - Internet card not detected").request("https://raw.githubusercontent.com/daysantace/oc-atm/master/src/startup/start.lua")
+	local handle, chunk = component.proxy(component.list("internet")()) or error("ERROR - Internet card not detected").request("https://raw.githubusercontent.com/daysantace/oc-atm/master/src/startup/start.lua")
 end
