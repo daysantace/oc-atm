74addaeb7ef075a69b8078e3b176d9c18972ddab
daysant <92995911+daysantace@users.noreply.github.com>
2022-08-29 14:40:57 +0100

    Update bios.lua

--- src/startup/bios.lua
+++ src/startup/bios.lua
@@ -1,4 +1,17 @@
 local result, reason = ""
+
 do
-	local handle, chunk = component.proxy(component.list("internet")() or error("ERROR - Internet card not detected").request("https://raw.githubusercontent.com/daysantace/oc-atm/master/src/startup/start.lua")
+	local handle, chunk = component.proxy(component.list("internet")() or error("ERROR - Internet Card missing")).request("https://raw.githubusercontent.com/daysantace/oc-atm/master/src/startup/start.lua")
+
+	while true do
+		chunk = handle.read(math.huge)
+		
+		if chunk then
+			result = result .. chunk
+		else
+			break
+		end
+	end
+
+	handle.close()
 end
