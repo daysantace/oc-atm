258aaa577c80626601a991c8bf641cb72532694c
daysantace@gmail.com <daysantace@gmail.com>
2022-10-21 19:09:13 +0100

    fixed stuff and adding card reading

--- main.lua
+++ main.lua
@@ -1,39 +1,27 @@
 -- get libs
-local component=require(component)
-local computer=require(computer)
-
---check for components
-if computer.totalMemory()<384000 then
-  term.clear()
-  print("ERROR! Not enough memory")
-end
-
-if not component.isAvaliable("modem") then
-  print("ERROR! Network card not detected")
-end
-
-if not component.isAvaliable("data") then
-  print("ERROR! Data card not detected")
-end
-
+local component=require("component")
+local computer=require("computer")
+local event=require("event")
 local modem=component.getPrimary("modem")
 local data=component.getPrimary("data")
 
+event.shouldInterrupt = function()
+  return false
+end
 
 while true do
-  
-  -- header
-  print("OC-ATM 0.0.1")
-  print("")
-  print("1 - Log in")
-  print("2 - Sign up")
-  local inp=io.read()
-  
-  if inp=="1" then -- log in
-    computer.beep(750,0.25)
-    print("Enter username")
-    local id=io.read()
-    
-    local pwd=io.read()
-  end
+    print("Please insert your card.")
+    function readCard(playerName, cardData)
+      usernamecheck = playerName
+      cardsplicing = cardData
+    end
+    event.listen("magData", readCard())
+    event.pull("interrupted")
+    event.ignore("magData", readCard())
+    -- split string
+    username=string.sub(cardsplicing,1,16)
+    cardnum=string.sub(cardsplicing,17,40)
+    cardID=string.sub(cardsplicing,41,80)
+    accID=string.sub(cardsplicing,81,120)
+    checksum=string.sub(cardsplicing,128,128)
 end
