d74689a4be5f6a55b5c60bdf0cd4fdbfcb18650c
daysant <92995911+daysantace@users.noreply.github.com>
2022-10-29 12:29:54 +0100

    finished deposit

--- main.lua
+++ main.lua
@@ -1,6 +1,7 @@
 -- config
 port = -- port for wireless networking, match it up with whatever the ATM is using
 currency = "minecraft:diamond" -- ID of currency (by default diamonds)
+atmAddress = -- address of the atm
 
 -- import libs
 require ("component")
@@ -12,12 +13,38 @@ inventory = component.inventory_controller
 
 -- wait for input from ATM
 while True do
-  local portMessage, _, messageCmd = event.pull("modem_message")
+  portMessage, _, messageCmd = event.pull("modem_message")
   if portMessage == port then
+-- deposit
     if messageCmd = "req_deposit" then
--- move back to chest
+  -- move back to chest
       move(2)
       move(2)
       currencyDep = 0
--- read items in hopper
-      inventory.getStackInSlot(2,1)
+  -- read items in hopper and take
+      for i=1, 5 then
+        slotCheck = inventory.getStackInSlot(2,i)
+        if slotCheck["name"] = currency then
+          currencyDep + slotCheck["size"] = currencyDep
+          suckFromSlot(2, i, slotCheck["size"])
+        end
+      end
+  -- send message to ATM and deposit diamonds
+      move(3)
+      move(3)
+      modem.send(atmAddress, port, tostring(currencyDep))
+      for i=1, 5 then
+        robot.select(i)
+        currencyDep = robot.count(i)
+        for j=1, 27 then
+          slotCheck = inventory.getStackInSlot(3,i)
+          if not slotCheck["size"] == slotCheck["maxsize"] then
+            slotDep = slotCheck["maxsize"] - slotCheck["size"]
+            dropIntoSlot(3,j,slotDep)
+          end
+        end
+      end
+    end
+  end
+-- withdraw
+end
