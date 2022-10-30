dac94ec7a85c748aa79a20618fc44bfb81c2c3a1
daysant <92995911+daysantace@users.noreply.github.com>
2022-10-30 23:25:55 +0000

    more stuff

--- src/installer.lua
+++ src/installer.lua
@@ -5,8 +5,21 @@ local os = require("os")
 
 if not component.isAvailable("internet") then
   print("The OC-ATM installer requires an internet card.")
+  io.read()
   os.exit()
 end
 
 -- get input for what to install
 print("OC-ATM Installer")
+print("Please type the respective number to install file.")
+print("1 - ATM")
+print("2 - ATM Robot")
+print("3 - Server")
+print("4 - Till")
+print("5 - Writer")
+print("6 - Relay")
+
+inp = io.read()
+
+-- install ATM
+pcall(internet.request, url, nil, {["user-agent"]="Wget/OpenComputers"})
