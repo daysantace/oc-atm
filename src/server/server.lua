c5a0856a6b940504a73afa811e50180cfba9587b
daysantace <daysantace@gmail.com>
2022-11-03 09:54:15 +0000

    added some server stuff + DRY

--- src/server/server.lua
+++ src/server/server.lua
@@ -11,27 +11,24 @@ local modem = component.proxy(component.modem.address)
 local data = component.proxy(component.data.address)
 
 -- init
+function getMsg(port){
+    modem.open(port)
+    local _, port .. "sender", _, _, bal = event.pull("modem_message")
+    modem.close(port)
+}
+
 while True do
-    modem.open(portPublic)
-    local _, sender, _, _, modemMessage = event.pull("modem_message")
+    getMsg(portPublic)
     if string.sub(modemMessage,1,6) == "req_bal" then
         -- balance request
-        modem.send(portPrivate,dataAddress,"req_bal" .. string.sub(modemMessage,7,string.len(modemMessage)))
-        modem.open(portPrivate)
-        local _, dataAddressConfirm, _, _, bal = event.pull("modem_message")
-        modem.close(portPrivate)
-        if dataAddressConfirm == dataAddress then
+        modem.send(portPrivate,dataAddress,"req_bal" .. string.sub(modemMessage,8,string.len(modemMessage)))
+        getMsg(portPrivate)
+        if portPrivate .. "sender" == dataAddress then
             modem.send(portPublic,sender,bal)
         end
     end
 
     if string.sub(modemMessage,1,4) == "with" then
         -- withdrawal
-        modem.send(portPrivate,dataAddress,"req_bal" .. string.sub(modemMessage,7,string.len(modemMessage)))
-        modem.open(portPrivate)
-        local _, dataAddressConfirm, _, _, bal = event.pull("modem_message")
-        modem.close(portPrivate)
-        if dataAddressConfirm == dataAddress then
-            modem.send(portPublic,sender,bal)
-        end
+        modem.send(portPrivate,dataAddress,"deduct" .. string.sub(modemMessage,7,string.len(modemMessage)))
     end
