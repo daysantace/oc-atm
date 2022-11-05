b7cd77331892ed4a5c6212970a770e92dcc360f3
daysantace <daysantace@gmail.com>
2022-11-05 12:02:45 +0000

    w

--- src/server/database.lua
+++ src/server/database.lua
@@ -1,6 +1,6 @@
 -- config
-serverAddress = -- server address
-port = -- port
+local serverAddress = -- server address
+local port = -- port
 
 -- get libs
 local event = require("event")
@@ -11,3 +11,11 @@ local data = component.proxy(component.data.address)
 
 -- init
 while True do
+    modemMessage = nil
+    modem.open(port)
+    local _, reqAddress, _, _, modemMessage = event.pull("modem_message")
+    modem.close(port)
+    if not modemMessage == nil then
+        if string.sub(modemMessage,1,7) == "req_bal" then
+            balInfo = string.sub(modemMessage,8,string.len(modemMessage))
+            
